09c371e3a16f02095e7be2f5c3c2ec1509843ae0
Hugo Martin <hugomrt.94@gmail.com>
2022-06-20 00:29:26 +0200

    git add for previous commit

---
+++ src/components/modal.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function Modal({ item, closeModal }) {
+  return (
+    <div className="modalBackground">
+      <div className="modalContent">
+        <div className="flex column left">
+          <div className="carousel-item">
+            <img src={item.url} alt={item} />
+          </div>
+          <img className="logo" src={item.logo} alt={item} />
+        </div>
+        <div className="flex column right">
+          <div className="flex row space-between">
+            <h1>{item.title}</h1>
+            <button
+              className="button align-self start"
+              onClick={() => closeModal(false)}
+            >
+              X
+            </button>
+          </div>
+          <p>{item.description}</p>
+          {item.see_more !== "" && (
+            <a
+              href={item.see_more}
+              className="button primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Voir plus
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
