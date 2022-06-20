f923cb71b523e84e3e3306cb7359210f5e4a97c3
Hugo Martin <hugomrt.94@gmail.com>
2022-06-20 19:54:37 +0200

    carousel with arrows

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,36 +1,64 @@
-import { React, useState } from 'react'
+import { React, useState } from "react";
 
-import Layout from '../components/layout/layout'
-import Modal from '../components/modal'
-import data from './data.json'
+import Layout from "../components/layout/layout";
+import Modal from "../components/modal";
+import data from "./data.json";
 
 const Portfolio = () => {
-  const [openModal, setOpenModal] = useState(false)
-  const [item, setItem] = useState(null)
+  const [openModal, setOpenModal] = useState(false);
+  const [item, setItem] = useState(null);
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(4);
 
   const items = data.map((item) => {
     return (
       <button
         key={item.id}
-        className='carousel-item button no-style'
+        className="carousel-item button no-style"
         onClick={() => {
-          setOpenModal(true)
-          setItem(item)
+          setOpenModal(true);
+          setItem(item);
         }}
       >
         <img src={item.url} alt={item} />
       </button>
-    )
-  })
+    );
+  });
 
   return (
     <Layout>
-      <div className='content'>
-        <div className='carousel-list'>{items}</div>
+      <div className="content">
+        <div className="arrow up">
+          {startIndex > 0 && (
+            <button
+              className="button no-style"
+              onClick={() => {
+                setStartIndex(startIndex - 4);
+                setEndIndex(endIndex - 4);
+              }}
+            >
+              <img src="up-arrow.svg" alt="up-arrow"></img>
+            </button>
+          )}
+        </div>
+        <div className="carousel-list">{items.slice(startIndex, endIndex)}</div>
+        <div className="arrow down">
+          {endIndex < data.length && (
+            <button
+              className="button no-style"
+              onClick={() => {
+                setStartIndex(startIndex + 4);
+                setEndIndex(endIndex + 4);
+              }}
+            >
+              <img src="down-arrow.svg" alt="down-arrow"></img>
+            </button>
+          )}
+        </div>
       </div>
       {openModal && <Modal item={item} closeModal={setOpenModal} />}
     </Layout>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
